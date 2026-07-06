26bbbdf7aa3220947a458d6d1c1ff66407f0f603
João Pedro de Lima <jpjpjplima@gmail.com>
2026-07-06 11:55:43 -0300

    FIX apenas relocando metodo findAll()

--- frontend/src/app/features/login-page/login-page.ts
+++ frontend/src/app/features/login-page/login-page.ts
@@ -1,6 +1,4 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Incidents } from '../../core/models/incidents-model';
-import { IncidentsService } from '../../core/services/incidents-service'
 import { RouterLink } from '@angular/router';
 // import { DatePipe } from '@angular/common';
 
@@ -10,24 +8,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage implements OnInit{
-
-  private IncidentsService = inject(IncidentsService);
-
-  incidents = signal<Incidents[]>([]);
-
-  ngOnInit(): void {
-    this.incidentsFindAll();
-  }
-
-  incidentsFindAll(): void {
-    this.IncidentsService.findAll().subscribe({
-      next: (res: any) => {
-        const data = res.content;
-        return this.incidents.set(data);
-      },
-      error: (err) => console.error(err)
-    });
-  }
+export class LoginPage {
 
 }
--- frontend/src/app/features/workspace-page/workspace-page.ts
+++ frontend/src/app/features/workspace-page/workspace-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Incidents } from '../../core/models/incidents-model';
+import { IncidentsService } from '../../core/services/incidents-service'
 
 @Component({
   selector: 'app-workspace-page',
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './workspace-page.html',
   styleUrl: './workspace-page.css',
 })
-export class WorkspacePage {}
+export class WorkspacePage implements OnInit {
+
+  private IncidentsService = inject(IncidentsService);
+
+  incidents = signal<Incidents[]>([]);
+
+  ngOnInit(): void {
+    this.incidentsFindAll();
+  }
+
+  incidentsFindAll(): void {
+    this.IncidentsService.findAll().subscribe({
+      next: (res: any) => {
+        const data = res.content;
+        return this.incidents.set(data);
+      },
+      error: (err) => console.error(err)
+    });
+  }
+}

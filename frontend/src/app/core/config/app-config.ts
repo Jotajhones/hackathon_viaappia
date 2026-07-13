cdb9623d3e42ead7f46edbdb6ee375e32e44e573
João Pedro de Lima <163007612+Jotajhones@users.noreply.github.com>
2026-07-13 13:54:58 -0300

    FIX - pequenas correções

--- frontend/src/app/core/config/app-config.ts
+++ frontend/src/app/core/config/app-config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from '../services/auth-interceptor'; // Ajuste o caminho se necessário
-import { routes } from '../../app.routes'; // Importe suas rotas aqui!
+import { authInterceptor } from '../services/auth-interceptor'; 
+import { routes } from '../../app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
